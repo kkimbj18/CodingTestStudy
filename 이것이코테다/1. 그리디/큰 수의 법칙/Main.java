f0f57f003e39ceb1ca580c89f390e1c513964c1c
kkimbj18 <kkimg3802@gmail.com>
2021-07-03 19:58:07 +0900

    큰 수의 법칙

---
+++ 이것이코테다/1. 그리디/큰 수의 법칙/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] temp = scan.nextLine().split(" ");
+        
+        int n = Integer.parseInt(temp[0]);
+        int m = Integer.parseInt(temp[1]);
+        int k = Integer.parseInt(temp[2]);
+
+        String[] numTemp = scan.nextLine().split(" ");
+        int[] num = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            num[i] = Integer.parseInt(numTemp[i]);
+        }
+
+        int sum = 0;
+
+        Arrays.sort(num);
+
+        int big = num[n - 1];
+        int sBig = num[n - 2];
+
+        sum = m * big - (m / (k + 1)) * (big - sBig);
+
+        System.out.println(sum);
+    }
+}
