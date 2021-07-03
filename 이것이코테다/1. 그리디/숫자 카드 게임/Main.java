1d64c28fc30f980c411f75e759db7116896b3a67
kkimbj18 <kkimg3802@gmail.com>
2021-07-04 00:50:20 +0900

    큰 수의 법칙, 숫자 카드 게임

---
+++ 이것이코테다/1. 그리디/숫자 카드 게임/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        int M = scan.nextInt();
+
+        int[][] num = new int[N][M];
+
+        for (let i = 0; i < N; i++) {
+            for (let j = 0; j < M; j++) {
+                num[i][j] = scan.nextInt();
+            }
+            Arrays.sort(num[i]);
+        }
+
+        int max = 0;
+
+        for (let i = 0; i < N; i++) {
+            if (num[i][0] > max) {
+                max = num[i][0];
+            }
+        }
+        
+        System.out.println(max);
+    }
+}
