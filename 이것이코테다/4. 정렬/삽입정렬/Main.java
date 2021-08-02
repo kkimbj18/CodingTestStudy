5536a29a5132392349f9963eb428ba6408bb8fa3
kkimbj18 <kkimg3802@gmail.com>
2021-08-02 23:49:40 +0900

    Sort: 삽입정렬

---
+++ 이것이코테다/4. 정렬/삽입정렬/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        sc.close();
+
+        for (int i = 1; i < N; i++) {
+            for (int j = i; j >= 0; j--) {
+                if (arr[j] < arr[j-1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                } else
+                    break;
+            }
+        }
+
+        for (int item : arr) {
+            System.out.println(item);
+        }
+    }
+}
