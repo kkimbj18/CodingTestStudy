58f068e1bc2c0aa71249cd7060ce3392237530e8
kkimbj18 <kkimg3802@gmail.com>
2022-01-18 22:48:32 +0900

    Programmers: Heap: 더_맵게 - Java로 재풀이

---
+++ src/Programmers/Heap/더_맵게/Solution.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        
+        for (int s: scoville) {
+            minHeap.add(s);
+        }
+        
+        if (minHeap.peek() >= K) return answer;
+        
+        while(true) {
+            answer++;
+            int[] minFoods = new int[2];
+            
+            minFoods[0] = minHeap.poll();
+            minFoods[1] = minHeap.poll();
+            
+            int newFood = minFoods[0] + (minFoods[1] * 2);
+            minHeap.add(newFood);
+            if (minHeap.peek() > K) break;
+            
+            if (minHeap.size() == 1) {
+                answer = -1;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
