a5d2be3628e5398f52f642703ca2b0a383eae399
kkimbj18 <kkimg3802@gmail.com>
2022-01-26 20:09:55 +0900

    Programmers: 그리디: 체육복 - Java로 재풀이

---
+++ src/Programmers/Greedy/체육복/Solution.java
@@ -0,0 +1,37 @@
+package Programmers.Greedy.체육복;
+
+// import java.util.Arrays;
+
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = n;
+
+        int[] arr = new int[n+2];
+
+        for (int l: lost) {
+            arr[l]--;
+        }
+        for (int r: reserve) {
+            arr[r]++;
+        }
+
+        // Arrays.sort(lost);
+
+        for (int l = 1; l <= n; l++) {
+            if (arr[l] != -1) continue;
+
+            if (arr[l-1] == 1) {
+                arr[l-1]--;
+            } else if (arr[l+1] == 1) {
+                arr[l+1]--;
+            } else {
+                answer--;
+                continue;
+            }
+
+            arr[l]++;
+        }
+
+        return answer;
+    }
+}
