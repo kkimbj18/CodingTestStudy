0b4ee11b1738ec98e9eb5d7942b0e6676ab20b39
kkimbj18 <kkimg3802@gmail.com>
2022-04-04 00:41:58 +0900

    백준 Unknown - 1149 S1 RGB거리

--- 2022/BaekJoon/Unknown/1149_S1_RGB거리/Main.java
+++ 2022/BaekJoon/Unknown/1149_S1_RGB거리/Main.java
@@ -1,3 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-    
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] costs = new int[N][3];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            for (int j = 0; j < 3; j++) {
+                costs[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int[][] dp = new int[N][3];
+
+        dp[0][0] = costs[0][0];
+        dp[0][1] = costs[0][1];
+        dp[0][2] = costs[0][2];
+
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < 3; j++) {
+                dp[i][j] = Math.min(dp[i-1][(j+1)%3], dp[i-1][(j+2)%3]) + costs[i][j];
+            }
+        }
+
+        int answer = Integer.MAX_VALUE;
+        for (int i = 0; i < 3; i++) {
+            answer = Math.min(answer, dp[N-1][i]);
+        }
+
+        System.out.println(answer);
+    }
 }
